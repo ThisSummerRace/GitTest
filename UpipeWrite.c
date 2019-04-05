63fc6ccfa94e446c46e59e8a2a8b373848a7d92a
ThisSummerRace <1174933862@qq.com>
2019-04-05 19:17:36 +0800

    GitTest

---
+++ UpipeWrite.c
@@ -0,0 +1,37 @@
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<errno.h>
+#include<stdio.h>
+#include<fcntl.h>
+#include<string.h>
+#include<stdlib.h>
+#define FIFO_PATH "myfifofile"
+int main(int argc,char *argv[])
+{
+    int fd;
+	char cont_w[] = "hello sundy!" ;
+    if(mkfifo(FIFO_PATH,0666) < 0 && errno != EEXIST)
+	{
+		perror("create fifo failed");
+		return -1;
+	}
+	else
+	{
+		printf("create fifo sucess\n");
+		fd = open(FIFO_PATH,O_CREAT|O_WRONLY,0666);
+		if(fd > 0)
+		{
+		    while(1)
+			{
+				write(fd,cont_w,strlen(cont_w));
+				printf("write success");
+				sleep(2);
+			}
+			close(fd);
+		}
+		else
+		{
+			perror("open failed!");
+		}
+	}
+}
